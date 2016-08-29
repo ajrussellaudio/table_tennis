bd702425d9784f517bf81e70d6793259261736e9
Alan Russell <ajrussellaudio@gmail.com>
2016-08-29 21:51:46 +0100

    starts on the sinatra stuff

---
+++ main.rb
@@ -0,0 +1,9 @@
+require "sinatra"
+require "sinatra/contrib/all"
+
+require "pry-byebug"
+
+require_relative "./models/game"
+require_relative "./models/player"
+require_relative "./controllers/game"
+
